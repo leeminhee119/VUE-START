1c0c9b2a3b436885d8fdf35c4b1da0ced05d6766
이민희 <mhl98@dgu.ac.kr>
2023-08-02 03:01:55 +0900

    feat: 기본 예제

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,45 @@
 <template>
-  <h1 
-    v-if="msg" 
-    class="message"
-    @click="log">
-    {{ msg }}
-  </h1>
-  <Hello />
+  <h1>설문조사</h1>
+  <TextField 
+    v-model="fields[0].value"
+    :title="fields[0].title" />
+  <SimpleRadio
+    v-model="fields[1].value"
+    :title="fields[1].title"
+    :items="fields[1].items" />
+  <h1>결과</h1>
+  <div
+    v-for="field in fields"
+    :key="field.title">
+    {{ field.value }}
+  </div>
 </template>
 
 <script>
-import Hello from '~/components/Hello'
+import TextField from '~/components/TextField'
+import SimpleRadio from '~/components/SimpleRadio'
 
 export default {
-  components: {
-    Hello,
-  },
-  data() {
-    return {
-      msg: 'Hello Vue!',
+    components: {
+      TextField, 
+      SimpleRadio,
+    },
+    data() {
+      return {
+        fields: [
+          {
+            component: 'TextField',
+            title: '이름',
+            value: '',
+          },
+          {
+            component: 'SimpleRadio',
+            title: '나이대',
+            value: '',
+            items: ['20대', '30대', '40대', '50대']
+          }
+        ]
+      }
     }
-  },
 }
 </script>
---
+++ src/components/TextField.vue
@@ -0,0 +1,23 @@
+<template>
+  <h3>{{ title }}</h3>
+  <input 
+    :value="modelValue"
+    type="text"
+    @input="$emit('update:modelValue', $event.target.value)" />
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: '',
+        },
+        modelValue: {
+            type: String,
+            default: '',
+        },
+    },
+    emits: ['update:modelValue']
+}
+</script>
