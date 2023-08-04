4cbc5900f6ccf143514ed734ac59b677c82c916b
이민희 <mhl98@dgu.ac.kr>
2023-08-04 23:14:30 +0900

    feat: 로그인 예제 - 스토어 구성

---
+++ src/store/user.js
@@ -0,0 +1,23 @@
+export default {
+  state() {
+    return {
+      isLoggedIn: false,
+    }
+  },
+  mutations: {
+    updateLoggedIn(state, payload) {
+      state.isLoggedIn = payload
+    }
+  },
+  actions: {
+    logIn({commit}, payload) {
+      const {userId, userPw} = payload
+      if (userId && userPw) {
+        commit('updateLoggedIn', true)
+      }
+    },
+    logOut({commit}) {
+      commit('updateLoggedIn', false)
+    }
+  }
+}
