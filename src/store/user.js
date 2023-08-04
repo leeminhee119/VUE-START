db0507876680e9554054dd4df6edf960610bb23b
이민희 <mhl98@dgu.ac.kr>
2023-08-05 00:06:40 +0900

    feat: 로그인 예제 - 로그아웃했을 때 리다이렉트 로직 추가

--- src/store/user.js
+++ src/store/user.js
@@ -18,7 +18,7 @@ export default {
       if (userId && userPw) {
         commit('updateLoggedIn', true)
 
-        const redirect = router.currentRoute.value.query.redirect // router 객체를 통해서도 currentRoute.value를 통해 route정보를 가져올 수 있다.
+        const {redirect} = router.currentRoute.value.query // router 객체를 통해서도 currentRoute.value를 통해 route정보를 가져올 수 있다.
         if (redirect) {
           router.push(redirect)
         } else {
@@ -28,6 +28,12 @@ export default {
     },
     logOut({commit}) {
       commit('updateLoggedIn', false)
+      
+      // 현재 페이지가 로그인이 필요한 페이지면, 홈으로 리다이렉트시킨다.
+      const {requiresAuth} = router.currentRoute.value.meta // 설정 안 해준 라우트는 undefined
+      if (requiresAuth) {
+        router.push('/')
+      }
     }
   }
 }
