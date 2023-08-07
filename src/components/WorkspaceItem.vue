de8e88962aed4144fbc993602915289fa286d1c0
이민희 <mhl98@dgu.ac.kr>
2023-08-07 15:54:41 +0900

    feat: 도큐먼트 데이터 store생성

---
+++ src/components/WorkspaceItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <li>
+    <div 
+      :style="{ paddingLeft: `${14}px`}"
+      class="title">
+      <span class="material-icons">
+        play_arrow
+      </span>
+      <span class="text">
+        {{ workspace.title }}
+      </span>
+      <div class="actions">
+        <span class="material-icons">
+          add
+        </span>
+        <span class="material-icons">
+          delete
+        </span>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    workspace: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+li {
+  .title {
+    &:hover {
+      background-color: $color-background--hover1;
+    }
+    .text {
+
+    }
+    .actions {
+
+    }
+    .material-icons {
+
+    }
+  }
+}
+</style>
