594b61c46e3303e42eaaedd1e8f3c5e35606cbe9
이민희 <mhl98@dgu.ac.kr>
2023-08-07 16:48:55 +0900

    feat: 하위 페이지 펼치기, 스타일링

--- src/components/WorkspaceItem.vue
+++ src/components/WorkspaceItem.vue
@@ -1,13 +1,16 @@
 <template>
   <li>
     <div 
-      :style="{ paddingLeft: `${14}px`}"
+      :style="{ paddingLeft: `${14 * depth}px`}"
       class="title">
-      <span class="material-icons">
+      <span
+        :class="{ active: showChildren }"
+        class="material-icons"
+        @click="showChildren = !showChildren">
         play_arrow
       </span>
       <span class="text">
-        {{ workspace.title }}
+        {{ workspace.title || '제목 없음' }}
       </span>
       <div class="actions">
         <span class="material-icons">
@@ -18,6 +21,19 @@
         </span>
       </div>
     </div>
+    <div 
+      v-if="!hasChildren && showChildren"
+      :style="{ paddingLeft: `${14 * depth + 22}px`}"
+      class="no-children">
+      하위 페이지가 없습니다.
+    </div>
+    <ul v-if="showChildren && hasChildren">
+      <WorkspaceItem
+        v-for="ws in workspace.documents"
+        :key="ws.id"
+        :depth="depth + 1"
+        :workspace="ws" />
+    </ul>
   </li>
 </template>
 
@@ -27,6 +43,20 @@ export default {
     workspace: {
       type: Object,
       default: () => ({})
+    },
+    depth: {
+      type: Number,
+      default: 1
+    }
+  },
+  data() {
+    return {
+      showChildren: false,
+    }
+  },
+  computed: {
+    hasChildren() {
+      return this.workspace.documents && this.workspace.documents.length
     }
   }
 }
@@ -34,19 +64,48 @@ export default {
 
 <style lang="scss" scoped>
 li {
+  color: $color-font;
   .title {
+    display: flex;
+    align-items: center;
+    padding-right: 10px;
+    height: 27px;
     &:hover {
       background-color: $color-background--hover1;
+      .actions {
+        display: flex;
+      }
     }
     .text {
-
+      flex-grow: 1;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
     }
     .actions {
-
+      display: none;
+      align-items: center;
     }
     .material-icons {
-
+      font-size: 20px;
+      color: $color-icon;
+      margin-right: 3px;
+      &:hover {
+        background: $color-background--hover2;
+      }
+      &.active {
+        transform: rotate(90deg);
+      }
     }
+  }
+  .no-children {
+    color: rgba($color-font, .35);
+    height: 30px;
+    display: flex;
+    align-items: center;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
   }
 }
 </style>
